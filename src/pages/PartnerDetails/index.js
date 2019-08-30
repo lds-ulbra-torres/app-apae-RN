2e9549682c9b6b073539c3014ee15de0aebe09a6
LucielVitorBS <luciel.vitor.bs@gmail.com>
2019-08-30 03:22:24 -0300

    cabeçalho detalhe do parceiro

--- src/pages/PartnerDetails/index.js
+++ src/pages/PartnerDetails/index.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {
     View,
     TouchableOpacity,
     SafeAreaView,
     Text,
     ImageBackground,
+    Image
 } from 'react-native';
 
 import styles from './styles';
 import { getPartnerDetails, baseURL } from '../../services/Api';
+import { Line } from '../Components';
+
+
 
 export default function PartnerDetails({ navigation }) {
     const id  = navigation.getParam('id');
@@ -26,9 +31,19 @@ export default function PartnerDetails({ navigation }) {
 
     return (
         <SafeAreaView style={ styles.container }>
-            <ImageBackground style={ styles.image } source= {{ uri: `${baseURL}${partner.photo_partner}` }}>
-                <Text> {partner.fantasy_name_partner} </Text>
-            </ImageBackground>
+            <Image style={ styles.image } source= {{ uri: `${baseURL}${partner.photo_partner}` }} />
+
+            <View style={ styles.name }>
+                <View style={ styles.containerFantasyName }>
+                    <Text style={ styles.fantasyName }> {partner.fantasy_name_partner} </Text>
+                </View>
+
+                <View style={ styles.containerCall }>
+                    <TouchableOpacity style={ styles.buttonCall }>
+                        <Icon name= 'phone' size= { 35 } color= '#4caf50'/>
+                    </TouchableOpacity>
+                </View>
+            </View>
 
             <Text>{partner.id_partner}</Text>
         </SafeAreaView>
--- src/pages/PartnerDetails/styles.js
+++ src/pages/PartnerDetails/styles.js
@@ -23,6 +23,42 @@ const styles = StyleSheet.create({
         height: '100%',
         maxHeight: 240,
     },
+    name: {
+        height: 'auto',
+        minHeight: 60,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#4caf50',
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+    },
+    containerFantasyName:{
+        flex: 3,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    containerCall:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonCall: {
+        height: 50,
+        width: 50,
+        borderRadius: 25,
+        borderColor: 'white',
+        borderWidth: 2,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    fantasyName: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: 'white'
+    },
 });
 
 export default styles;
